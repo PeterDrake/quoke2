c381f6a3eb6f0ef1efc03195959c2d7d8715096b
Peter Drake <drake@lclark.edu>
2021-02-11 09:00:32 -0800

    Initial commit.

---
+++ Assets/Scripts/Comment.cs
@@ -0,0 +1,8 @@
+﻿// This can be attached to any GameObject as a place for a comment visible within Unity
+// Idea from https://www.codeproject.com/Tips/1208852/How-to-Add-Comments-Notes-to-a-GameObject-in-Unity
+using UnityEngine;
+
+public class Comment : MonoBehaviour
+{
+    [TextArea] public string notes = "Type comment here.";
+}
