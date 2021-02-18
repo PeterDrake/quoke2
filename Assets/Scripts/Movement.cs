4eae56828f8c346c169958f93e3eb520e966dfd5
Shohrukh Jalolov <sjalolov@lclark.edu>
2021-02-18 14:31:35 -0800

    We added Movement Script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float panSpeed = 20f;
+
+    void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * Time.deltaTime;
+        }
+    }
+}
