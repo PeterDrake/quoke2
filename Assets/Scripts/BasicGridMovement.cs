1e618a2626acf784faf23ee57cbaba93becabb32
Blythe <blytheballesteros@gmail.com>
2021-03-06 01:20:14 -1000

    added a collectable layer for items that can be picked up. added pick up function to movement script,added collectables to scene

--- Assets/Scripts/BasicGridMovement.cs
+++ Assets/Scripts/BasicGridMovement.cs
@@ -12,20 +12,28 @@ public class BasicGridMovement : MonoBehaviour
     public Transform movePoint;
     public bool isMoving;
 
-    static string[] collisionLayers = {"Wall"};
-    int layersToCollideWith;
+    public LayerMask layersToCollideWith;
+    public LayerMask layersToCollectFrom;
 
     public GameObject visualCheck;
+    private Collider[] hitColliders;
     private int numberOfCollidersFound;
+    private Collider[] hitCollectables;
+    private int numberofCollectablesFound;
+
+    public List<GameObject> CollectedItems;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        layersToCollideWith = LayerMask.GetMask(collisionLayers);
-        Debug.Log(layersToCollideWith);
         isMoving = false;
         movePoint.parent = null; // So that moving player doesn't move its child movePoint
         visualCheck.transform.position = transform.position;
+        hitColliders = null;
+        hitCollectables = null;
+        CollectedItems = new List<GameObject>();
     }
 
 
@@ -39,11 +47,16 @@ public class BasicGridMovement : MonoBehaviour
             if (Math.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
                 numberOfCollidersFound = findColliders("Horizontal");
+                numberofCollectablesFound = findCollectables("Horizontal");
+                // Debug.Log("Colliders = " + numberOfCollidersFound + " Collectables = " + numberofCollectablesFound);
+
                 moveHorizontally();           
             }
             else if (Math.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
                 numberOfCollidersFound = findColliders("Vertical");
+                numberofCollectablesFound = findCollectables("Vertical");
+                // Debug.Log("Colliders = " + numberOfCollidersFound + " Collectables = " + numberofCollectablesFound);
                 moveVertically(); 
             }
             else
@@ -56,8 +69,6 @@ public class BasicGridMovement : MonoBehaviour
     //returns number of colliders stored in hitCollider array while using OverlapSphere function in specific direction
     int findColliders(string direction)
     {
-        Collider[] hitColliders = null;
-
         if (direction == "Horizontal")
         {   
             hitColliders = Physics.OverlapSphere(movePoint.position + 
@@ -76,12 +87,38 @@ public class BasicGridMovement : MonoBehaviour
             visualCheck.transform.position = movePoint.position + new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
         }
         return hitColliders.Length;    
-
     }
 
+    //returns number of colliders stored in hitCollectables array
+    int findCollectables(string direction)
+    {
+        if (direction == "Horizontal")
+        {   
+            hitCollectables = Physics.OverlapSphere(movePoint.position + 
+                new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .5f, layersToCollectFrom);
+        }
+        if (direction == "Vertical")
+        {
+            hitCollectables = Physics.OverlapSphere(movePoint.position + new Vector3(0f, 0f, Input.GetAxisRaw("Vertical")),
+                    0.5f, layersToCollectFrom);
+        }
+        return hitCollectables.Length;  
+    }
+
+    
+
     // Will face direction of movement & if no colliders are found in direction, will update movePoint to new positioning in horizontal direction
+    // if a collectable is infront will add it to CollectedItems List and deactivate and then move to that spot if able
     void moveHorizontally()
     {
+        if (numberofCollectablesFound > 0)
+        {
+            foreach (Collider element in hitCollectables)
+            {
+                CollectedItems.Add(element.gameObject);
+                element.gameObject.SetActive(false);
+            }
+        }
         if (numberOfCollidersFound == 0)
         {
             movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
@@ -91,8 +128,17 @@ public class BasicGridMovement : MonoBehaviour
     }
 
     // Will face direction of movement & if no colliders are found in direction, will update movePoint to new positioning in vertical direction
+    // if a collectable is infront will add it to CollectedItems List and deactivate and then move to that spot if able
     void moveVertically()
     {
+        if (numberofCollectablesFound > 0)
+        {
+            foreach (Collider element in hitCollectables)
+            {
+                CollectedItems.Add(element.gameObject);
+                element.gameObject.SetActive(false);
+            }
+        }
         if (numberOfCollidersFound == 0)
         {
             movePoint.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
