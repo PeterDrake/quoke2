8a6525e66c59a2fa7889d5fb2679428c7f304115
Blythe <blytheballesteros@gmail.com>
2021-03-07 13:43:32 -1000

    added collectable item script

---
+++ Assets/CollectableItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItem : MonoBehaviour
+{
+    private GameObject player;
+    void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player").gameObject;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")){
+            Debug.Log("pick me");
+            other.GetComponent<BasicGridMovement>().collectables.Add(this.gameObject);
+            gameObject.SetActive(false);
+
+        }
+    }
+
+    public void DropItem()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        transform.position = player.transform.position + player.transform.forward;
+        player.GetComponent<BasicGridMovement>().collectables.Remove(this.gameObject);
+    }
+}
