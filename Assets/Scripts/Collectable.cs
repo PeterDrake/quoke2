3ad72db5316db1195d1791ba703e937464cbe499
Terin <terintrachtenberg@lclark.edu>
2021-03-18 14:21:04 -0700

    Currently trying to get items to be picked up when walked into; still early in the attempt

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -12,6 +12,14 @@ public class Collectable : MonoBehaviour
     }
 
     //REPLACE with OnTriggerEnter(Collider other) method to incorporate movement
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Ran into item");
+        }
+    }
     void Update()
     {
         //if B key is pressed, add a item to the inventory
