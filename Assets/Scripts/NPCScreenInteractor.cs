f2f3ee83792f63f188596b66785a71232ade3515
garrett bosse <garrettbosse@gmail.com>
2021-03-18 12:53:38 -0800

    we can now change what the npc says based on what the player says, next step is getting the convo arrays to change based on what player says

--- Assets/Scripts/NPCScreenInteractor.cs
+++ Assets/Scripts/NPCScreenInteractor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class OpenNPCScreen : MonoBehaviour
+public class NPCScreenInteractor : MonoBehaviour
 {
 
     public Button button1;
@@ -13,15 +13,19 @@ public class OpenNPCScreen : MonoBehaviour
     public Button button3;
     public int cursorLocation = 0;
     public string[] textArray;
+    public string[] npcArray;
     public Button[] buttons;
+    public GameObject npcText;
     
     
     
         // Start is called before the first frame update
         private void Start()
         {
+            npcArray = new[] {"You said 'button 1'", "You said 'button 2'", "You said 'button 3'"};
             textArray = new[] {"Hey here's text for button 1", "Hey here's text for button 2", "Hey here's text for button 3"};
             buttons = new Button[] {button1, button2, button3};
+            
         }
 
         void Update()
@@ -54,6 +58,7 @@ public class OpenNPCScreen : MonoBehaviour
         
         if (Input.GetKeyDown("space"))
         {
+            npcText.GetComponentInChildren<Text>().text = npcArray[cursorLocation];
             buttons[cursorLocation].GetComponentInChildren<Text>().text = textArray[cursorLocation];
         }
         
