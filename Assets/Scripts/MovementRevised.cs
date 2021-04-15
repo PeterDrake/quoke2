f1cc6ea3061d7bfb290ca69d59a2a04f708db98d
0tpyrk <ninjo07@gmail.com>
2021-04-14 22:40:05 -0700

    Add tables

--- Assets/Scripts/MovementRevised.cs
+++ Assets/Scripts/MovementRevised.cs
@@ -8,10 +8,12 @@ using UnityEngine.SceneManagement;
 
 public class MovementRevised : MonoBehaviour
 {
+    public GameObject playerHead;
 
     public float moveSpeed = 5f;
     public Transform movePoint;
     public bool moving;
+    public bool crouching;
     
     
     int layersToCollideWith;
@@ -19,7 +21,7 @@ public class MovementRevised : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        layersToCollideWith = LayerMask.GetMask("Wall", "NPC");
+        layersToCollideWith = LayerMask.GetMask("Wall", "NPC", "Table");
         Debug.Log("Mask: " + layersToCollideWith);
         moving = false;
         movePoint.parent = null; // So that moving player doesn't move its child movePoint
@@ -33,6 +35,15 @@ public class MovementRevised : MonoBehaviour
             movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal") * 0.5f, 0.0f, 0.0f),
             0.2f, layersToCollideWith);
 
+        // Exception if the player is crouching and the object in front is a table
+        if (hitColliders.Length == 1)
+        {
+            if (hitColliders[0].gameObject.layer == 10 && crouching)
+            {
+                return false;
+            }
+        }
+
         return hitColliders.Length != 0;
     }
     
@@ -42,6 +53,15 @@ public class MovementRevised : MonoBehaviour
         // Get all things in the space on the grid the player is trying to move to
         Collider[] hitColliders = Physics.OverlapSphere(movePoint.position + new Vector3(0f, 0.0f, Input.GetAxisRaw("Vertical") * 0.5f),
             0.2f, layersToCollideWith);
+
+        // Exception if the player is crouching and the object in front is a table
+        if (hitColliders.Length == 1)
+        {
+            if (hitColliders[0].gameObject.layer == 10 && crouching)
+            {
+                return false;
+            }
+        }
 
         return hitColliders.Length != 0;
     }
@@ -91,6 +111,18 @@ public class MovementRevised : MonoBehaviour
             {
                 moving = false;
             }
+        }
+
+        Collider[] tableCheckColliders = Physics.OverlapSphere(transform.position, 0.2f, layersToCollideWith);
+        if (Input.GetKey(KeyCode.C) || tableCheckColliders.Length != 0)
+        {
+            crouching = true;
+            playerHead.transform.localPosition = new Vector3(playerHead.transform.localPosition.x, 0f, playerHead.transform.localPosition.z);
+        }
+        else
+        {
+            crouching = false;
+            playerHead.transform.localPosition = new Vector3(playerHead.transform.localPosition.x, 0.5f, playerHead.transform.localPosition.z);
         }
     }
 }
