da167a346315c2b864b7b8b4f727e828ab741046
Peter Drake <drake@lclark.edu>
2021-05-09 13:39:06 -0700

    Cleaned up TitleScene.

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    public string FirstScene;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(FirstScene);
+        }
+    }
+    
+}
