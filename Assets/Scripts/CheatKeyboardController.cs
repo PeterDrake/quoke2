5528e1f695087c9244ce844f4d6d09551db72b65
Peter Drake <drake@lclark.edu>
2021-05-23 11:44:03 -0700

    Cleaned up Yard.

--- Assets/Scripts/CheatKeyboardController.cs
+++ Assets/Scripts/CheatKeyboardController.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class CheatKeyboardController : MonoBehaviour
 {
+    // For scenes with no meters (before and during the quake), this can remain null
+    public Meters meters;
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
@@ -16,6 +19,14 @@ public class CheatKeyboardController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.M))
         {
             SceneManager.LoadScene("QuakeHouse");
+        }
+        if (meters && Input.GetKeyDown(KeyCode.O))
+        {
+            meters.MarkTaskAsDone("water");
+        }
+        if (meters && Input.GetKeyDown(KeyCode.P))
+        {
+            meters.MarkTaskAsDone("poop");
         }
     }
 }
