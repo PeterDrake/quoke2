c335c62e5398a00fd27b5622f7ee971e30338d52
Isaiah Nduku <isaiah@lclark.edu>
2021-05-27 15:49:24 -0500

    Added tips for the player on the death screen

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class PlayerDeath : MonoBehaviour
 {
-    public GameObject textChange;
+    public GameObject DeathText;
+    public GameObject TipsText;
     public bool playerDeath;
     private GameObject player;
     //private GameObject CallingObject;
@@ -23,10 +24,16 @@ public class PlayerDeath : MonoBehaviour
         switch (x)
         {
             case 0:
-                textChange.GetComponent<Text>().text = "You got crushed!";
+                DeathText.GetComponent<Text>().text = "You got crushed!";
+                TipsText.GetComponent<Text>().text = "Get under a solid structure during the earthquake!";
                 break;
             case 1:
-                textChange.GetComponent<Text>().text = "You died in an aftershock!";
+                DeathText.GetComponent<Text>().text = "You died in an aftershock!";
+                TipsText.GetComponent<Text>().text = "Don't re-enter unstable structures!";
+                break;
+            case 2:
+                DeathText.GetComponent<Text>().text = "You died of thirst!";
+                TipsText.GetComponent<Text>().text = "Regular access to clean water is vital!";
                 break;
         }
     }
