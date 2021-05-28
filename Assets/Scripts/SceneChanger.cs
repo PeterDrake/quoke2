350205c370fa895c6837e9b9c489cf3b1467cac0
RossGLPT <rmiyabuchi@lclark.edu>
2021-05-28 10:30:01 -1000

    Unsuccessfully tried to make a persistent inventory by looking at Inventory's item array.

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,13 +14,28 @@ public class SceneChanger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            
             // Set GlobalControls to current scene
             string previousScene = SceneManager.GetActiveScene().name;
             switch (previousScene)
             {
                 case "School":
+                    Inventory inventory = GameObject.Find("Inventory").GetComponent<Inventory>();    
                     GlobalControls.CurrentScene = 0;
+                    foreach (var collectible in GlobalControls.ItemList)
+                    {
+                        if (collectible.scene.Equals("School"))
+                        {
+                            if (collectible.prefab.activeInHierarchy)
+                            {
+                                collectible.location = collectible.prefab.transform.position;
+                            }
+                            else
+                            {
+                                collectible.scene = "Inventory";
+                                //collectible.location = new Vector3(Array.IndexOf(inventory.items));
+                            }
+                        }
+                    }
                     break;
                 case "Park":
                     GlobalControls.CurrentScene = 1;
