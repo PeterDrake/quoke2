336f26f224f71fd5f4c7935203988801caf56379
RossGLPT <rmiyabuchi@lclark.edu>
2021-06-21 08:09:02 -1000

    Title Screen now starts game properly.

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,10 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
 
     public string FirstScene;
+    public string currentScene;
+
+    public void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+    }
+
+    public void Update()
+    {
+        if (currentScene.Equals("TitleScreen") && Input.GetKeyDown("space"))
+        {
+            RestartGame();
+        }
+    }
 
     public void RestartGame()
     {
