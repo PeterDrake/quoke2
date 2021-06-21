1ba5924b5633f381a2476b56baa7d08e9a97cfd6
Skye Russ <skyer@lclark.edu>
2021-06-21 16:01:05 -0700

    XML File exception data at the root is invalid. <3

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -36,8 +36,7 @@ public class DialogueManager : MonoBehaviour
         UnityWebRequest request = UnityWebRequest.Get(filepath);
         Debug.Log("Attempting to communicate with server at " + filepath);
         yield return request.SendWebRequest();
-        yield return new WaitForSeconds(0.5f);
-        
+
         if (request.result != UnityWebRequest.Result.Success)
         {
             Debug.Log("ERROR ERROR Download Failed <3");
@@ -46,27 +45,18 @@ public class DialogueManager : MonoBehaviour
         else
         {
             Debug.Log("WOOHOO DOWNLOAD SUCCEEDED <3");
-            Debug.Log(request.downloadHandler.text);
-            Debug.Log("File Contents = " + fileContents);
             fileContents = request.downloadHandler.text;
+            Debug.Log("File Contents = " + fileContents);
         }
 
         
         request.Dispose();
-    }
-    public void BeginConversation()
-    {
         
-        //Paste the path of the xml file you want to look at here
-        filepath = Application.streamingAssetsPath + "/2TestTree.xml";
+        Debug.Log("Loading XML File");
 
-        if (filepath.Contains("http"))
-        {
-            StartCoroutine(GetWebText());
-            convoFile.LoadXml(fileContents);
-        }
-        else convoFile.LoadXml(File.ReadAllText(filepath)); 
-        
+        Debug.Log(convoFile);
+        convoFile.LoadXml(fileContents);
+
         //looks through all the npc nodes instead of looking at just the <convoForest> tag
         foreach (XmlNode node in convoFile.LastChild) 
         {
@@ -96,6 +86,54 @@ public class DialogueManager : MonoBehaviour
         //This displays the initial nodes npc text
         npcText.GetComponentInChildren<Text>().text = currentNode.npcText; 
         buttons[cursorLocation].Select();
+    }
+
+    public void BeginConversation()
+    {
+        
+        //Paste the path of the xml file you want to look at here
+        filepath = Application.streamingAssetsPath + "/2TestTree.xml";
+
+        if (filepath.Contains("http"))
+        {
+            StartCoroutine(GetWebText());
+        }
+        else
+        {
+            convoFile.LoadXml(File.ReadAllText(filepath));
+            
+            //looks through all the npc nodes instead of looking at just the <convoForest> tag
+            foreach (XmlNode node in convoFile.LastChild) 
+            {
+                if (!forest.ContainsKey(node.Name))
+                {
+                    forest.Add(node.Name, new ConvoNode(node));
+                }
+            }
+
+            // This is where the we let the NPC talk to the code. The npc we run into will pass back something like
+            // "theirName0" to get to the appropriate starting node
+            currentNode = forest[GlobalControls.NPCList[GlobalControls.CurrentNPC].node]; 
+        
+            for (int c = 0; c < currentNode.playerArray.Count; c++)
+            {
+                buttons[c].gameObject.SetActive(true);
+            
+                //This displays the initial nodes player text
+                buttons[c].GetComponentInChildren<Text>().text = currentNode.playerArray[c];     
+            
+                //Turns a button off if there is no text in the button
+                if (buttons[c].GetComponentInChildren<Text>().text.Equals(""))
+                {
+                    buttons[c].gameObject.SetActive(false);
+                }
+            }
+            //This displays the initial nodes npc text
+            npcText.GetComponentInChildren<Text>().text = currentNode.npcText; 
+            buttons[cursorLocation].Select();
+        } 
+        
+        
     }
     
     public int ChangeCursorLocations(int location)
