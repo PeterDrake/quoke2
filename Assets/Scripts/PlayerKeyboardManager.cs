43183f2e48f69b6ae4c8d55158d846b1269e58a0
RossGLPT <rmiyabuchi@lclark.edu>
2021-06-21 08:26:11 -1000

    Refractored PlayerKeyboardManager so all the booleans are just one integer gamemode.

--- Assets/Scripts/PlayerKeyboardManager.cs
+++ Assets/Scripts/PlayerKeyboardManager.cs
@@ -7,18 +7,16 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class PlayerKeyboardManager : MonoBehaviour
 {
-    public PlayerMover player;
-    public Inventory inventory;
-    public bool isExploring;
-    public bool isTrading;
-    public bool isConversing;
-    public bool isSegue;
-    public bool isDeath;
-    public ReferenceManager referenceManager;
-    public TradeManager tradeManager;
-    public DialogueManager dialogueManager;
-    public GameObject deathCanvas;
-    public GameObject segueCanvas;
+    private PlayerMover player;
+    private Inventory inventory;
+
+    private int gamemode; //{1 = segue, 2 = conversing, 3 = exploring, 4 = death, 5 = trading}
+    
+    private ReferenceManager referenceManager;
+    private TradeManager tradeManager;
+    private DialogueManager dialogueManager;
+    private GameObject deathCanvas;
+    private GameObject segueCanvas;
 
     private int cursorLocation;
     // Note that the 1 key is at index 0, and so on. This neatly accounts for 0-based array index and doesn't have to be
@@ -48,130 +46,150 @@ public class PlayerKeyboardManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isSegue)
+        if (gamemode == 1)
         {
-            if (Input.GetKeyDown("space"))
-            {
-                SetExploring();
-            }
+            UpdateSegue();
         }
-        else if (isDeath)
+        else if (gamemode == 2) //if talking to an npc
         {
-            if (Input.GetKeyDown("space"))
-            {
-                deathCanvas.GetComponent<StartButton>().RestartGame();
-            }
+            UpdateConversing();
         }
-        else if (isExploring) //if moving around the map
+        else if (gamemode == 3) //if moving around the map
         {
-            // Crouch (c)
-            player.SetCrouching(Input.GetKey(KeyCode.C));
-            // Move (wasd or arrow keys)
-            float h = Input.GetAxisRaw("Horizontal");
-            float v = Input.GetAxisRaw("Vertical");
-            if (Math.Abs(h) >= 1f)
-            {
-                player.StartMoving(new Vector3(h, 0f, 0f));
-            } else if (Math.Abs(v) >= 1f)
-            {
-                player.StartMoving(new Vector3(0f, 0f, v));
-            }
-            
-            
-            // Select from inventory (1-9)
-            for (int i = 0; i < validInputs.Length; i++)
-            {
-                if (inventory && Input.GetKey(validInputs[i]))
-                {
-                    inventory.SelectSlotNumber(i);
-                }
-            }
-            if (inventory && Input.GetKeyDown(KeyCode.Space))
-            {
-                inventory.PickUpOrDrop();
-            }
+            UpdateExploring();
         } 
-        else if (isConversing) //if talking to an npc
+        else if (gamemode == 4) //if ded
         {
-            // Change the cursor's location with < and >
-            if (Input.GetKeyDown(","))
-            {
-                cursorLocation--;
-                cursorLocation = dialogueManager.ChangeCursorLocations(cursorLocation);
-            }
-            if (Input.GetKeyDown("."))
-            {
-                cursorLocation++;   
-                cursorLocation = dialogueManager.ChangeCursorLocations(cursorLocation);
-            }
-            
-        
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {  
-                SetExploring();
-            }
-        
-            //Selects an option from the player options
-            if (Input.GetKeyDown("space"))
-            {
-                dialogueManager.EncapsulateSpace();
-                cursorLocation = dialogueManager.ChangeCursorLocations(0);
-            }
+            UpdateDeath();
         } 
-        else if (isTrading) //if trading with an npc
+        else if (gamemode == 5) //if trading with an npc
         {
-            if (Input.GetKeyDown(","))
-            {
-                cursorLocation--;
-                cursorLocation = tradeManager.ChangeSelectedInventory(cursorLocation);
-                
-            }
-            if (Input.GetKeyDown("."))
-            {
-                cursorLocation++;
-                cursorLocation = tradeManager.ChangeSelectedInventory(cursorLocation);
-            }
-            
-            //select slots
-            for (int i = 0; i < validInputs.Length; i++)
-            {
-                if (Input.GetKey(validInputs[i]))
-                {
-                    tradeManager.SelectSlot(cursorLocation, i);
-                }
-            }
-        
-        
-            // Transfer items (space)
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                tradeManager.EncapsulateSpace(cursorLocation);
-            }
+            UpdateTrading();
+        }
+    }
 
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                if (tradeManager.CheckValidTrade())
-                {
-                    Debug.Log("Valid trade!");
-                    tradeManager.CompleteTrade();
-                }
-                else Debug.Log("Invalid Trade!");
-            }
+    private void UpdateSegue()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SetExploring();
+        }
+    }
+    
+    private void UpdateConversing()
+    {
+        // Change the cursor's location with < and >
+        if (Input.GetKeyDown(","))
+        {
+            cursorLocation--;
+            cursorLocation = dialogueManager.ChangeCursorLocations(cursorLocation);
+        }
+        if (Input.GetKeyDown("."))
+        {
+            cursorLocation++;   
+            cursorLocation = dialogueManager.ChangeCursorLocations(cursorLocation);
+        }
             
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {  
-                tradeManager.LeaveTrading();
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {  
+            SetExploring();
+        }
+        
+        //Selects an option from the player options
+        if (Input.GetKeyDown("space"))
+        {
+            dialogueManager.EncapsulateSpace();
+            cursorLocation = dialogueManager.ChangeCursorLocations(0);
+        }
+    }    
+    
+    private void UpdateExploring()
+    {
+        // Crouch (c)
+        player.SetCrouching(Input.GetKey(KeyCode.C));
+        // Move (wasd or arrow keys)
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        if (Math.Abs(h) >= 1f)
+        {
+            player.StartMoving(new Vector3(h, 0f, 0f));
+        } else if (Math.Abs(v) >= 1f)
+        {
+            player.StartMoving(new Vector3(0f, 0f, v));
+        }
+            
+            
+        // Select from inventory (1-9)
+        for (int i = 0; i < validInputs.Length; i++)
+        {
+            if (inventory && Input.GetKey(validInputs[i]))
+            {
+                inventory.SelectSlotNumber(i);
             }
+        }
+        if (inventory && Input.GetKeyDown(KeyCode.Space))
+        {
+            inventory.PickUpOrDrop();
+        }
+    }
+    
+    private void UpdateDeath()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            deathCanvas.GetComponent<StartButton>().RestartGame();
+        }
+    }
+    private void UpdateTrading()
+    {
+        if (Input.GetKeyDown(","))
+        {
+            cursorLocation--;
+            cursorLocation = tradeManager.ChangeSelectedInventory(cursorLocation);
+                
+        }
+        if (Input.GetKeyDown("."))
+        {
+            cursorLocation++;
+            cursorLocation = tradeManager.ChangeSelectedInventory(cursorLocation);
+        }
+            
+        //select slots
+        for (int i = 0; i < validInputs.Length; i++)
+        {
+            if (Input.GetKey(validInputs[i]))
+            {
+                tradeManager.SelectSlot(cursorLocation, i);
+            }
+        }
+        
+        
+        // Transfer items (space)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tradeManager.EncapsulateSpace(cursorLocation);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (tradeManager.CheckValidTrade())
+            {
+                Debug.Log("Valid trade!");
+                tradeManager.CompleteTrade();
+            }
+            else Debug.Log("Invalid Trade!");
+        }
+            
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {  
+            tradeManager.LeaveTrading();
         }
     }
     
     public void SetExploring()
     {
-        isExploring = true;
-        isConversing = false;
-        isTrading = false;
-        isSegue = false;
-        isDeath = false;
+        gamemode = 3;
         
         deathCanvas.SetActive(false);
         segueCanvas.SetActive(false);
@@ -183,11 +201,7 @@ public class PlayerKeyboardManager : MonoBehaviour
 
     public void SetConversing()
     {
-        isExploring = false;
-        isConversing = true;
-        isTrading = false;
-        isSegue = false;
-        isDeath = false;
+        gamemode = 2;
         cursorLocation = 0;
         
         deathCanvas.SetActive(false);
@@ -201,11 +215,7 @@ public class PlayerKeyboardManager : MonoBehaviour
 
     public void SetTrading()
     {
-        isExploring = false;
-        isConversing = false;
-        isTrading = true;
-        isSegue = false;
-        isDeath = false;
+        gamemode = 5;
         cursorLocation = 0;
         
         deathCanvas.SetActive(false);
@@ -219,11 +229,7 @@ public class PlayerKeyboardManager : MonoBehaviour
 
     public void SetSegue()
     {
-        isExploring = false;
-        isConversing = false;
-        isTrading = false;
-        isSegue = true;
-        isDeath = false;
+        gamemode = 1;
 
         referenceManager.player.GetComponent<PlayerMover>().enabled = false;
         referenceManager.inventoryCanvas.SetActive(false);
@@ -237,11 +243,7 @@ public class PlayerKeyboardManager : MonoBehaviour
 
     public void SetDeath()
     {
-        isExploring = false;
-        isConversing = false;
-        isTrading = false;
-        isSegue = false;
-        isDeath = true;
+        gamemode = 4;
 
         referenceManager.player.GetComponent<PlayerMover>().enabled = false;
         referenceManager.inventoryCanvas.SetActive(false);
