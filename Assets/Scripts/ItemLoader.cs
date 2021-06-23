48dc2adff654322bc0631734e2b94b2d18bdb903
RossGLPT <rmiyabuchi@lclark.edu>
2021-06-23 06:03:53 -1000

    Deleted old comments.

--- Assets/Scripts/ItemLoader.cs
+++ Assets/Scripts/ItemLoader.cs
@@ -9,19 +9,9 @@ public class ItemLoader : MonoBehaviour
     private Inventory inventory;
     private StorageContainer container;
 
-    //weknowwhatstartdoesthx
     void Start()
     {
         
-        /*DEBUG
-        foreach (Item value in GlobalItemList.ItemList.Values)
-        {
-            //Debug.Log(value.name + " in " + value.scene + " at " + value.location + " with container " + value.containerName);
-            Debug.Log(value.ToString());
-        }*/
-        
-        
-        //can i keep it?
         GameObject g = GameObject.FindWithTag("Inventory");
         if (g)
         {
@@ -31,7 +21,6 @@ public class ItemLoader : MonoBehaviour
         {
             if (item.scene.Equals(SceneManager.GetActiveScene().name) && item.containerName.Equals(""))
             {
-                // Debug.Log("When item is not in storage :" + item.containerName);
                 //poot item here
                 GameObject prefab = (GameObject)Resources.Load(item.name, typeof(GameObject));
                 // prefab.transform.position = item.location;
@@ -43,7 +32,6 @@ public class ItemLoader : MonoBehaviour
             {
                 //populate inventory with many things
                 GameObject prefab = (GameObject)Resources.Load(item.name, typeof(GameObject));
-                // inventory.PickUpAtSlot((int) item.location.x, prefab);
                 GameObject itemInInventory = Instantiate(prefab, item.location, Quaternion.identity);
                 inventory.PickUpAtSlot((int) item.location.x, itemInInventory);
             }
@@ -60,7 +48,6 @@ public class ItemLoader : MonoBehaviour
                     itemInScene.transform.position = item.location;
                     
                     //place this item into the storage container's contents
-                    // Debug.Log("<" + item.containerName + ">");
                     itemInContainer.GetComponent<StorageContainer>().contents = itemInScene;
 
                 }
