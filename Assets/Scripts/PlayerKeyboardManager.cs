85e74e846719a75c764f0f3d025fc57daaec4e7d
Skye Russ <skyer@lclark.edu>
2021-07-01 12:49:41 -0700

    Fixed meters being in scene when restart. <3

--- Assets/Scripts/PlayerKeyboardManager.cs
+++ Assets/Scripts/PlayerKeyboardManager.cs
@@ -371,6 +371,7 @@ public class PlayerKeyboardManager : MonoBehaviour
         segueCanvas.SetActive(false);
         referenceManager.player.GetComponent<PlayerMover>().enabled = true;
         if(GlobalControls.MetersEnabled) referenceManager.metersCanvas.SetActive(true);
+        else if(!GlobalControls.MetersEnabled) referenceManager.metersCanvas.SetActive(false);
         referenceManager.dialogueCanvas.SetActive(false);
         referenceManager.tradeCanvas.SetActive(false);
         if (GlobalControls.ObjectivesEnabled)
@@ -408,6 +409,7 @@ public class PlayerKeyboardManager : MonoBehaviour
                 npcFrames[i].GetComponent<Image>().sprite = unselected;
             }
         }
+        else if(!npcInteractedInScene) referenceManager.npcInteractedCanvas.SetActive(false);
 
         
     }
