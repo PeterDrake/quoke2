dd8beeaba129c9a3597fe1f64b0b89c4bb699bb0
Peter Drake <drake@lclark.edu>
2021-07-07 11:38:54 -0700

    Commented out all tests to file a bug report with Unity.

--- Assets/Tests/TestInventory.cs
+++ Assets/Tests/TestInventory.cs
@@ -5,9 +5,9 @@ using UnityEngine.TestTools;
 
 public class TestInventory
 {
-    private GameObject levelPrefab;
-
-    private ReferenceManager referenceManager;
+    // private GameObject levelPrefab;
+    //
+    // private ReferenceManager referenceManager;
     
     // [SetUp]
     // public void Setup()
@@ -25,49 +25,49 @@ public class TestInventory
     //     Object.Destroy(levelPrefab);
     // }
     
-    [UnityTest]
-    public IEnumerator Cabinet1IsInitiallyEmpty
-        ()
-    {
-        // Setup
-        levelPrefab = MonoBehaviour.Instantiate(Resources.Load<GameObject>("PreQuakeHouse Level"));
-        referenceManager = GameObject.Find("Managers").GetComponent<ReferenceManager>();
-        // // Because the test scene doesn't have the expected name, it's necessary to call LoadItems directly
-        // ItemLoader itemLoader = referenceManager.itemLoader.GetComponent<ItemLoader>();
-        // itemLoader.LoadItems("PreQuakeHouse");
-        GameObject cabinet = GameObject.Find("Cabinet 1");
-        Assert.Null(cabinet.GetComponent<StorageContainer>().contents);
-        yield return null;
-        Object.Destroy(levelPrefab);
-    }
+    // [UnityTest]
+    // public IEnumerator Cabinet1IsInitiallyEmpty
+    //     ()
+    // {
+    //     // Setup
+    //     // levelPrefab = MonoBehaviour.Instantiate(Resources.Load<GameObject>("PreQuakeHouse Level"));
+    //     // referenceManager = GameObject.Find("Managers").GetComponent<ReferenceManager>();
+    //     // // // Because the test scene doesn't have the expected name, it's necessary to call LoadItems directly
+    //     // // ItemLoader itemLoader = referenceManager.itemLoader.GetComponent<ItemLoader>();
+    //     // // itemLoader.LoadItems("PreQuakeHouse");
+    //     // GameObject cabinet = GameObject.Find("Cabinet 1");
+    //     // Assert.Null(cabinet.GetComponent<StorageContainer>().contents);
+    //     yield return null;
+    //     // Object.Destroy(levelPrefab);
+    // }
 
-    [UnityTest]
-    public IEnumerator PicksUpAnItem()
-    {
-        // Setup
-        levelPrefab = MonoBehaviour.Instantiate(Resources.Load<GameObject>("PreQuakeHouse Level"));
-        referenceManager = GameObject.Find("Managers").GetComponent<ReferenceManager>();
-        // Because the test scene doesn't have the expected name, it's necessary to call LoadItems directly
-        ItemLoader itemLoader = referenceManager.itemLoader.GetComponent<ItemLoader>();
-        itemLoader.LoadItems("PreQuakeHouse");
-        // Get various objects
-        Inventory inventory = referenceManager.inventoryCanvas.GetComponent<Inventory>();
-        PlayerMover player = referenceManager.player.GetComponent<PlayerMover>();
-        GameObject cup = GameObject.Find("Cup(Clone)");
-        Assert.NotNull(cup);
-        // Take some steps
-        for (int i = 0; i < 5; i++)
-        {
-            player.StartMoving(Vector3.forward);
-            yield return new WaitForSeconds(0.5f);
-        }
-        for (int i = 0; i < 2; i++)
-        {
-            player.StartMoving(Vector3.right);
-            yield return new WaitForSeconds(0.5f);
-        }
-        // Verify that the cup was picked up
-        Assert.AreEqual(cup, inventory.items[0]);
-        Object.Destroy(levelPrefab);
-    }
+    // [UnityTest]
+    // public IEnumerator PicksUpAnItem()
+    // {
+    //     // Setup
+    //     levelPrefab = MonoBehaviour.Instantiate(Resources.Load<GameObject>("PreQuakeHouse Level"));
+    //     referenceManager = GameObject.Find("Managers").GetComponent<ReferenceManager>();
+    //     // Because the test scene doesn't have the expected name, it's necessary to call LoadItems directly
+    //     ItemLoader itemLoader = referenceManager.itemLoader.GetComponent<ItemLoader>();
+    //     itemLoader.LoadItems("PreQuakeHouse");
+    //     // Get various objects
+    //     Inventory inventory = referenceManager.inventoryCanvas.GetComponent<Inventory>();
+    //     PlayerMover player = referenceManager.player.GetComponent<PlayerMover>();
+    //     GameObject cup = GameObject.Find("Cup(Clone)");
+    //     Assert.NotNull(cup);
+    //     // Take some steps
+    //     for (int i = 0; i < 5; i++)
+    //     {
+    //         player.StartMoving(Vector3.forward);
+    //         yield return new WaitForSeconds(0.5f);
+    //     }
+    //     for (int i = 0; i < 2; i++)
+    //     {
+    //         player.StartMoving(Vector3.right);
+    //         yield return new WaitForSeconds(0.5f);
+    //     }
+    //     // Verify that the cup was picked up
+    //     Assert.AreEqual(cup, inventory.items[0]);
+    //     Object.Destroy(levelPrefab);
+    // }
 }
