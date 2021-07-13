42392bdae4c114f93039c561e01c80e7f357d521
Peter Drake <drake@lclark.edu>
2021-07-13 08:47:32 -0700

    Junk

--- Assets/Scripts/Comment.cs
+++ Assets/Scripts/Comment.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Comment : MonoBehaviour
 {
+    // This line is a garbage comment just to change a file
     [TextArea(minLines: 2, maxLines: 32)]
     public string notes = "Type comment here.";
 }
