b5ef11a8e06d65f2dbb598ccc013be1cb3f29c70
RossGLPT <rmiyabuchi@lclark.edu>
2021-09-21 17:06:56 -0700

    Added another large if statement to InventoryUI. This one had to deal with using the [ and ] keys to move between NPCInteractedUI and InventoryUI.

--- Assets/Scripts/PlayerKeyboardManager.cs
+++ Assets/Scripts/PlayerKeyboardManager.cs
@@ -354,62 +354,12 @@ public class PlayerKeyboardManager : MonoBehaviour
         else if (keyDown.Equals(KeyCode.D)) player.StartMoving(new Vector3(1, 0, 0));
         
         // Select from inventory (1-9)
-        
-
-        // TODO: This probably should be in inventory functionality, as it is dropping or picking up an item.
         if (inventoryInScene && cursorLocation < inventory.items.Length  && keyDown.Equals(KeyCode.Space))
         {
             inventory.PickUpOrDrop();
         }
 
-        // TODO: This probably should be in Inventory UI, as moves the cursor between NPCInteracted UI and Player Inventory UI
-        if (inventory && npcInteractedCanvas.activeSelf 
-                      && (keyDown.Equals(KeyCode.RightBracket) || keyDown.Equals(KeyCode.LeftBracket)))
-        {
-            if (cursorLocation > inventory.items.Length - 1)
-            {
-                npcFrames[cursorLocation - inventory.items.Length].GetComponent<Image>().sprite = unselected;
-                inventoryUI.EnableSelectedSlot();
-                cursorLocation = 0;
-                inventory.SelectSlotNumber(cursorLocation);
-                
-                npcInventoryTooltip.SetActive(false);
-            }
-            else
-            {
-                npcFrames[0].GetComponent<Image>().sprite = selected;
-                inventoryUI.DisableSelectedSlot();
-                cursorLocation = inventory.items.Length;
-                inventory.SelectSlotNumber(2);
-                
-                if (GlobalControls.globalControlsProperties.Contains("tooltipsEnabled") && GlobalControls.npcList[npcList[0]].interacted)
-                {
-                    if(!tooltipText.gameObject.GetComponentInParent<Image>(true).gameObject.activeSelf)
-                        tooltipText.gameObject.GetComponentInParent<Image>(true).gameObject.SetActive(true);
-                    tooltipText.text = GlobalControls.npcList[npcList[0]].description;
-                    npcInventoryTooltip.SetActive(true);
-                    npcInventoryTooltipName.text = GlobalControls.npcList[npcList[cursorLocation - inventory.items.Length]].name + "'s Inventory";
-                    for (int j = npcInventoryTooltipSprites.Length - 1; j >= 0; j--)
-                    {
-                        npcInventoryTooltipSprites[j].sprite = unselected;
-                        npcInventoryTooltipItemName[j].text = "";
-                    }
-                    foreach (Item item in GlobalItemList.ItemList.Values)
-                    {
-                        if (item.scene.Equals("Inventory") && item.containerName.Equals(npcList[cursorLocation - inventory.items.Length]))
-                        {
-                            GameObject prefab = (GameObject) Resources.Load(item.name, typeof(GameObject));
-                            Sprite sprite = prefab.GetComponent<Collectible>().sprite;
-                            npcInventoryTooltipSprites[(int) item.location.x].sprite = sprite;
-                            npcInventoryTooltipItemName[(int) item.location.x].text = item.name;
-                        }
-                    }
-
-                    
-                }
-                else tooltipText.gameObject.GetComponentInParent<Image>(true).gameObject.SetActive(false);
-            }
-        }
+        
 
         // TODO: This probably should be in Inventory UI, as moves the cursor DOWN the Inventory Slots 
         if (inventory && keyDown.Equals(KeyCode.Period))
