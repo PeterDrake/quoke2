0e32abea183d70fd70a47dc5b4d763bd71cedf19
0tpyrk <ninjo07@gmail.com>
2021-09-23 20:19:22 -0700

    Fix formatting

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -4,7 +4,6 @@ using System.Xml;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 
 public class DialogueManager : MonoBehaviour
@@ -57,7 +56,6 @@ public class DialogueManager : MonoBehaviour
         for (int c = 0; c < currentNode.playerArray.Count; c++)
         {
             buttons[c].gameObject.SetActive(true);
-
             //This displays the initial nodes player text
             buttons[c].GetComponentInChildren<Text>().text = currentNode.playerArray[c];     
             
@@ -75,8 +73,9 @@ public class DialogueManager : MonoBehaviour
         {
             keyboardManager.leftTrading = false;
 
-            if (GlobalItemList.ItemList["First Aid Kit"].containerName.Equals("Angie") && 
-                !GlobalControls.globalControlsProperties.Contains("angieHasFirstAidKit") && GlobalControls.CurrentNPC.Contains("angie"))
+            if (GlobalItemList.ItemList["First Aid Kit"].containerName.Equals("Angie")
+                && !GlobalControls.globalControlsProperties.Contains("angieHasFirstAidKit")
+                && GlobalControls.CurrentNPC.Contains("angie"))
                 if(!GlobalControls.globalControlsProperties.Contains("angieSeriousDialogue"))
                 {                        
                     GlobalControls.SetCheckpoint("basic_angie_4.0");
@@ -89,8 +88,9 @@ public class DialogueManager : MonoBehaviour
                     currentNode = forest["leave_angie_1.4"];
                     GlobalControls.globalControlsProperties.Add("angieHasFirstAidKit");
                 }
-            else if (GlobalItemList.ItemList["Epi Pen"].containerName.Equals("Angie") && 
-                !GlobalControls.globalControlsProperties.Contains("angieHasEpiPen") && GlobalControls.CurrentNPC.Contains("angie"))
+            else if (GlobalItemList.ItemList["Epi Pen"].containerName.Equals("Angie")
+                     && !GlobalControls.globalControlsProperties.Contains("angieHasEpiPen")
+                     && GlobalControls.CurrentNPC.Contains("angie"))
                 if(!GlobalControls.globalControlsProperties.Contains("angieSeriousDialogue"))
                 {                        
                     GlobalControls.SetCheckpoint("basic_angie_8.0");
@@ -102,9 +102,11 @@ public class DialogueManager : MonoBehaviour
                     currentNode = forest["leave_angie_1"];
                     GlobalControls.globalControlsProperties.Add("angieHasEpiPen");
                 }
-            else if (!GlobalControls.globalControlsProperties.Contains("angieSeriousDialogue") && GlobalControls.CurrentNPC.Contains("angie"))
+            else if (!GlobalControls.globalControlsProperties.Contains("angieSeriousDialogue")
+                     && GlobalControls.CurrentNPC.Contains("angie"))
                 currentNode = forest["leave_angie_0"];
-            else if (GlobalControls.globalControlsProperties.Contains("angieSeriousDialogue") && GlobalControls.CurrentNPC.Contains("angie"))
+            else if (GlobalControls.globalControlsProperties.Contains("angieSeriousDialogue")
+                     && GlobalControls.CurrentNPC.Contains("angie"))
                 currentNode = forest["leave_angie_1"];
             else
                 currentNode = forest["leave_error"];
