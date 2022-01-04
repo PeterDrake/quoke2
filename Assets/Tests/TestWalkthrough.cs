d71d4ef54266166390e2f1226ab52fb44d6525a8
RossGLPT <rmiyabuchi@lclark.edu>
2022-01-04 14:26:09 -0800

    Refactored tests to separate parts of playthrough. Teleports to individual scenes separately from the actual actions in scene.

--- Assets/Tests/TestWalkthrough.cs
+++ Assets/Tests/TestWalkthrough.cs
@@ -34,28 +34,50 @@ public class TestWalkthrough
         }
         yield return null;
     }
-    
+
     [UnityTest]
     public IEnumerator GetsToQuake()
     {
+        SceneManager.LoadScene("PreQuakeHouse");
+        yield return MakesMovesToGetToQuake();
+    }
+    public IEnumerator MakesMovesToGetToQuake()
+    {
+        yield return new WaitForSeconds(1.5f);
+        referenceManager = GameObject.Find("Managers").GetComponent<ReferenceManager>();
+        playerKeyboard = referenceManager.keyboardManager.GetComponent<PlayerKeyboardManager>();
+
         yield return QuokeTestUtils.Press("aaawwaaaaaaadddssssssss aads ", playerKeyboard);
         Assert.AreEqual("QuakeHouse", SceneManager.GetActiveScene().name);
     }
+
     
     [UnityTest]
     public IEnumerator GetsOutsideAfterQuake()
     {
-        SceneManager.LoadScene("QuakeHouse"); 
-        
+        SceneManager.LoadScene("QuakeHouse");
+        yield return MakesMovesToGetOutsideAfterQuake();
+    }
+    public IEnumerator MakesMovesToGetOutsideAfterQuake()
+    {
         yield return new WaitForSeconds(1.5f);
         referenceManager = GameObject.Find("Managers").GetComponent<ReferenceManager>();
         playerKeyboard = referenceManager.keyboardManager.GetComponent<PlayerKeyboardManager>();
         gameStateManager = referenceManager.gameStateManager.GetComponent<GameStateManager>();
-        gameStateManager.SetExploring();
         
+        gameStateManager.SetExploring();
         yield return QuokeTestUtils.Press("cwwwwwwwwda", playerKeyboard);
         yield return new WaitForSeconds(5f);
         yield return QuokeTestUtils.Press("aasssaa", playerKeyboard);
         Assert.AreEqual("Yard", SceneManager.GetActiveScene().name);
+    }
+
+
+    [UnityTest]
+    public IEnumerator CompletesPlaythrough()
+    {
+        SceneManager.LoadScene("PreQuakeHouse");
+        yield return MakesMovesToGetToQuake();
+        yield return MakesMovesToGetOutsideAfterQuake();
     }
 }
